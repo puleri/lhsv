f1e075db907664709b323478073a1ec22665d374
Matt Puleri <71623797+puleri@users.noreply.github.com>
2026-03-08 13:33:46 -0700

    Add small projects portfolio category page

---
+++ src/app/portfolio/_data/smallProjectsPortfolioItems.js
@@ -0,0 +1,35 @@
+const smallProjectsPortfolioItems = [
+  {
+    name: "Madison Park Residence Remodel",
+    href: "/portfolio/madison-park-residence-remodel/",
+    image: "/wp-content/uploads/2020/05/MadisonParkRemodel2k-187.jpg",
+  },
+  {
+    name: "Queen Anne Bathroom",
+    href: "/portfolio/queen-anne-bathroom/",
+    image: "/wp-content/uploads/2014/05/QueenAnneBathTitle.jpg",
+  },
+  {
+    name: "Denny-Blaine",
+    href: "/portfolio/denny-blaine/",
+    image: "/wp-content/uploads/2014/05/dennyBlaine1.jpg",
+  },
+  {
+    name: "Madrona Residence",
+    href: "/portfolio/madrona-residence/",
+    image: "/wp-content/uploads/2014/05/madronaRes1.jpg",
+  },
+  {
+    name: "Windermere Arbors",
+    href: "/portfolio/windermere-arbors/",
+    image: "/wp-content/uploads/2014/05/windermereArbors10.jpg",
+  },
+  {
+    name: "Washington Park Residence",
+    href: "/portfolio/washington-park-residence/",
+    image: "/wp-content/uploads/2014/05/washingtonPark1.jpg",
+    position: "top center",
+  },
+];
+
+export default smallProjectsPortfolioItems;
